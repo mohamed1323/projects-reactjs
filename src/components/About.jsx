1205a4f4715a7b0168d752934c8e03e94cd596e2
mohamed1323 <114732963+mohamed1323@users.noreply.github.com>
2024-03-30 15:56:29 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,8 +11,8 @@ function About() {
           <h2>ABOUT ME</h2>
           <h1 id="who-am-i">hi  I'm </h1>
           <h2>MohamedAmiin</h2>
-          <p>A senior programer aiming    developing websites/mobileapps  </p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda mollitia temporibus itaque ut pariatur architecto quo at nobis quia illum inventore placeat cum harum, distinctio odio fugit unde praesentium repellendus dolor laudantium voluptate. Architecto a nesciunt fuga laboriosam provident, recusandae ut quae, earum, vero assumenda aut nihil ex fugiat vel.</p>
+           <p>waxa an ahay xirfadle kufiican  programer and i love it to do that and i'm aiming    developing websites/mobileapps  </p>
+          <p>and i hope inan noqdo xirfadle an faca dalkiisa iyo dadkiisa waxa ad ujeclahay inan cilmi baaris kasuubiyo technologyda iyo qeybaheda kala duwan .</p>
           <div>
           
           </div>
